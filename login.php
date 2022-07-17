5ff1873a6bc3a4331571d130558f2a85ff4f1030
MurimiCaroline <104681417+MurimiCaroline@users.noreply.github.com>
2022-07-17 03:40:06 +0300

    Update and rename login.html to login.php
    making css universal

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOGIN</title>
-    <link rel="stylesheet" href="css/index.css?t=6789">
+    <link rel="stylesheet" href="css/index.css<?php echo time();?>">
     <!--prevents quest for cached file-->
 </head>
 <body>
